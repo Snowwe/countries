d80e4ffe8823aab5c3ea177cb9da96b5a758cf16
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-03-19 14:52:35 +0300

    feat: add api-service tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,12 +1,64 @@
-import { TestBed } from '@angular/core/testing';
-
-import { ApiService } from './api.service';
+import {TestBed} from '@angular/core/testing';
+import {ApiService} from './api.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let http: HttpTestingController;
+  let service: ApiService;
+
+  const expectedData = [
+    {userId: '1', id: '11', title: 'First', completed: true},
+    {userId: '2', id: '22', title: 'Second', completed: true},
+    {userId: '3', id: '33', title: 'Third', completed: true},
+  ];
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [ApiService]
+    });
+
+    service = TestBed.get(ApiService);
+    http = TestBed.get(HttpTestingController);
+
+  });
+
+  afterEach(() => {
+    http.verify();
+  });
 
   it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
     expect(service).toBeTruthy();
+  });
+
+  it('should have made one request to GET data from expected URL', () => {
+
+    service.get().subscribe((data: object) => {
+      expect(data).toEqual(expectedData);
+    });
+    const req = http.expectOne(service.apiUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(expectedData);
+  });
+
+  it('get should return stubbed value from a spy', () => {
+    // create `get` spy on an object representing the ValueService
+    const valueServiceSpy =
+      jasmine.createSpyObj('ApiService', ['get']);
+
+    // set the value to return when the `get` spy is called.
+    const stubValue = 'stub value';
+    valueServiceSpy.get.and.returnValue(stubValue);
+
+    service = new ApiService(valueServiceSpy);
+
+    expect(service.get())
+      .toBe(stubValue, 'service returned stub value');
+    expect(valueServiceSpy.get.calls.count())
+      .toBe(1, 'spy method was called once');
+    expect(valueServiceSpy.get.calls.mostRecent().returnValue)
+      .toBe(stubValue);
   });
 });
